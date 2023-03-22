6a5df570a297633a087d91f05d0a1a222880b513
Andrey Dantas <andreydantasvf@gmail.com>
2023-03-22 15:09:53 -0300

    feat: create component Loading

---
+++ src/components/Loading/styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Spinner = styled.div`
+  width: 3rem;
+  height: 3rem;
+  border: 0.5rem solid gray;
+  border-top: 0.5rem solid black;
+  border-radius: 50%;
+  animation: spinner 1.5s linear infinite;
+  
+  @keyframes spinner {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
