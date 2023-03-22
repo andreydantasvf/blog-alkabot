33610f5b66b15e0f9a16489763f094ad3415a34f
Andrey Dantas <andreydantasvf@gmail.com>
2023-03-22 20:45:28 -0300

    feat: create theme css

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+export default {
+  COLORS: {
+    BACKGROUND_LIGHT: "#efefef",
+
+    PRIMARY_COLOR: "#28303d",
+
+    TEXT_100: "#fff",
+    TEXT_300: "#71767b",
+
+    GRAY: "#ccc",
+    BLACK: "#000",
+
+    INPUT_300: "#747474"
+  }
+}
