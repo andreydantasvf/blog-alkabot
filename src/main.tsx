0366f9e697caaf3faa6582ddf822c7588db3dffb
Andrey Dantas <andreydantasvf@gmail.com>
2023-03-21 23:01:37 -0300

    Create route to Home page

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import GlobalStyles from './styles/global';
 
+import { Routes } from './routes';
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <GlobalStyles />
-    <h1>Hello</h1>
+    <Routes />
   </React.StrictMode>,
 )
