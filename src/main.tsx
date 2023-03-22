355693cc72ba61eb77d27b4103c72a86164b2a24
Andrey Dantas <andreydantasvf@gmail.com>
2023-03-21 22:54:23 -0300

    Create global css

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GlobalStyles from './styles/global';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <GlobalStyles />
     <h1>Hello</h1>
   </React.StrictMode>,
 )
