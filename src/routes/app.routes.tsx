15deeacaed42595a8bdb1e1c33303463eb0f00b8
Andrey Dantas <andreydantasvf@gmail.com>
2023-03-22 13:20:41 -0300

    Create route to /post/:id

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 
 import { Home } from '../pages/Home';
+import { PostDetails } from '../pages/PostDetails';
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/post/:id' element={<PostDetails />} />
     </Routes>
   )
 }
