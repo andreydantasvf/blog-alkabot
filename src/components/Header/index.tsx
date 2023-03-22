f76e47b4c7c0da37b693b73d7a9e509397ecd0d1
Andrey Dantas <andreydantasvf@gmail.com>
2023-03-21 23:15:08 -0300

    Create Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles";
+
+export function Header() {
+  return (
+    <Container>
+      <h1>BLOG</h1>
+    </Container>
+  )
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  width: 100%;
+  height: 8rem;
+  grid-area: header;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background: linear-gradient(to right, #1e469a, #49a7c1);
+  color: #FFF;
+  font-size: 2rem;
+  letter-spacing: 1rem;
+`;
