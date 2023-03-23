346848facb9d7936f24e1a29f357ac0c4c043630
Andrey Dantas <andreydantasvf@gmail.com>
2023-03-23 13:32:23 -0300

    feat: create theme to app

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,17 @@
+export default {
+  title: 'light',
+
+  colors: {
+    primary: '#28303d',
+
+    background: '#efefef',
+
+    text_100: '#fff',
+    text_300: '#71767b',
+
+    gray: '#ccc',
+    black: '#000',
+
+    input_300: '#747474'
+  }
+}
