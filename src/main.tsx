a14c68cdf68fd3112d972b6953e228663c4597e7
Andrey Dantas <andreydantasvf@gmail.com>
2023-03-23 13:33:31 -0300

    feat: add theme in app

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/global';
 
+import theme from './styles/themes/light';
 import { Routes } from './routes';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <GlobalStyles />
-    <Routes />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Routes />
+    </ThemeProvider>
   </React.StrictMode>,
 )
